ddf342b22f1e9db0d81040fb5a437204e2300e98
viewfuhrer <atlantij@gmail.com>
2012-10-08 23:55:57 +0400

    production cfg

--- config/settings_by_env/production.py
+++ config/settings_by_env/production.py
@@ -7,7 +7,7 @@ DATABASES = {
         'NAME': 'atlantij_cittavita',                      # Or path to database file if using sqlite3.
         'USER': 'atlantij_cittavita',                      # Not used with sqlite3.
         'PASSWORD': 'b36b888c',                  # Not used with sqlite3.
-        'HOST': 'Web315',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
